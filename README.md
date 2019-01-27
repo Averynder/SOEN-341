63df31dc1548f1558bfb67d4dc827b63f23a0833
andrelegault <andre.legault1@gmail.com>
2019-01-27 17:49:07 +0000

    Fix index

---
+++ README.md
@@ -0,0 +1,23 @@
+# Concordia Course Planner
+[![Build Status](https://travis-ci.org/andrelegault/node-react.svg?branch=master)](https://travis-ci.org/andrelegault/node-react)
+
+### Installation
+
+The planner requires [Node.js](https://nodejs.org/) v8+ and [React](https://reactjs.org) to run.
+
+Install the dependencies for both Node.js and React.
+
+```sh
+$ yarn
+$ cd client
+$ yarn
+```
+
+Start Node.js on port 3001 and react on port 3000.
+```sh
+$ PORT=3001 node bin/www &
+$ cd client
+$ yarn start
+```
+
+Go to http://localhost:3000 and check to see if everything works.
---
+++ routes/users.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'test'
+});
+
+connection.connect();
+connection.query('select 1+1 as solution', function(error, results, fields) {
+    if(error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	// Comment out this line:
+  //res.send('respond with a resource');
+
+  // And insert something like this instead:
+  res.json([{
+  	id: 1,
+  	username: "samsepi0l"
+  }, {
+  	id: 2,
+  	username: "D0loresH4ze"
+  }]);
+});
+
+module.exports = router;
