109fb070461429740e784071412e75edf4d452fb
andrelegault <andre.legault1@gmail.com>
2019-01-27 01:38:15 +0000

    Add mysql support

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'test'
+});
+
+connection.connect();
+connection.query('select 1+1 as solution', function(error, results, fields) {
+    if(error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
