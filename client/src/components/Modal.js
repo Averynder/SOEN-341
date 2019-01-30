a702c0a1b66ba7c0ca86e1525ee2753da1b1aae2
Ali Mahram <alimahram@hotmail.com>
2019-01-30 18:58:55 -0500

    extra touch

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -8,19 +8,6 @@ class Modal extends React.Component{
         return null;
       }
 
-    const modalStyle = {
-      backgroundColor: '#FFF',
-      borderRadius: 5,
-      top: 0,
-      bottom: 10,
-      left: 0,
-      right: 0,
-      maxWidth: 500,
-      minHeight: 300,
-      display: "inline-block",
-      zIndex: 4,
-    };
-
     return (
         <div className="container" id="modal-container">
           <div className="modal-content">
@@ -29,25 +16,26 @@ class Modal extends React.Component{
               <h3>{this.props.userType}</h3>
               <button type="button" style={{margin: 0}} className="btn btn-dark" data-dismiss="modal" onClick={this.props.onClose}>&times;</button>
             </div>
+            <form>
+              <div className="modal-body" style={{textAlign: "center"}}>
+                <h4>Sign In</h4>
+                <br />
+                <div class="md-form mb-5">
+                  <h6 class="fas fa-envelope prefix grey-text">Netname:  </h6>
+                  <input className="input validate" type="text" id="defaultForm-email" placeholder="n_louis" required />
+                </div>
 
-            <div className="modal-body" style={{textAlign: "center"}}>
-              <h4>Sign In</h4>
-              <br />
-              <div class="md-form mb-5">
-                <h6 class="fas fa-envelope prefix grey-text">Netname:  </h6>
-                <input className="input validate" type="email" id="defaultForm-email"  />
+                <div class="md-form mb-5">
+                  <h6 class="fas fa-envelope prefix grey-text">Password:  </h6>
+                  <input className="input validate" type="password" id="defaultForm-email"  placeholder="********" required />
+                </div>
+
               </div>
 
-              <div class="md-form mb-5">
-                <h6 class="fas fa-envelope prefix grey-text">Password:  </h6>
-                <input className="input validate" type="email" id="defaultForm-email"  />
+              <div className="modal-footer">
+                <button type="submit" className="btn btn-dark" onClick/*Process Login*/>Login</button>
               </div>
-
-            </div>
-
-            <div className="modal-footer">
-              <button type="button" className="btn btn-dark" onClick/*Process Login*/>Login</button>
-            </div>
+            </form>
           </div>
         </div>
     )
