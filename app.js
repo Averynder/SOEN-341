7574bfe957f6d1f29282ee163918ce92b67ae773
Victor Manea <victor.manea@icloud.com>
2019-02-02 16:16:34 -0500

    Somewhat functional cookies are here. Next we'll need to link the given user ID to the user input credentials.

--- app.js
+++ app.js
@@ -2,14 +2,111 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
-// view engine setup
+var app = express();
+app.use(cookieParser());
+
+
+// set a cookie with random number as ID
+// we should link this id to the given username/password input
+app.use(function (req, res, next) {
+
+  // checks if the client has sent cookie
+  var cookie = req.cookies.cookieID;
+
+
+  // creates new cookies if there isn't already one
+  if (cookie === undefined)
+  {
+
+    // the username and password below should be eventually linked to the user input
+
+    var randomID = Math.floor(Math.random() * 9000000000) + 1000000000;
+
+    res.cookie('cookieID', randomID, { httpOnly: true });
+    // res.cookie('cookieID2', 'password', { maxAge: 1000000, httpOnly: true });
+
+    console.log('cookie has been set');
+
+
+  }
+  else
+  {
+    console.log('cookie already exists', cookie);
+  }
+  next();
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(3000, function () {
+  console.log('server started on port 3000');
+});
+
+// // set a cookie
+// // this version sets the username/password as their own cookie. This is, however, not safe.
+// app.use(function (req, res, next) {
+//
+//   // checks if the client has sent cookie
+//   var cookie = req.cookies.cookieID1;
+//
+//
+//   // creates new cookies if there isn't already one
+//   if (cookie === undefined)
+//   {
+//
+//     // the username and password below should be eventually linked to the user input
+//     res.cookie('cookieID1', 'username', { maxAge: 1000000, httpOnly: true });
+//     res.cookie('cookieID2', 'password', { maxAge: 1000000, httpOnly: true });
+//
+//     console.log('cookie has been set');
+//
+//
+//   }
+//   else
+//   {
+//     console.log('cookie already exists', cookie);
+//   }
+//   next();
+// });
+//
+// app.use(express.static(__dirname + '/public'));
+//
+//
+// app.listen(3000, function () {
+//   console.log('server started on port 3000');
+// });
+
+// Random testing
+// var logger = function(req, res, next){
+//   console.log('Logging...');
+//   next();
+// };
+// app.use(logger);
+
+// // Body Parser Middleware
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
+//
+// // Set Static Path
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.get('/', function (req, res) {
+//   res.send('lol');
+//   next();
+// });
+
+
+
+
+// view engine setup (keep this)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -27,7 +124,8 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+
+// error handler  (keep this)
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
