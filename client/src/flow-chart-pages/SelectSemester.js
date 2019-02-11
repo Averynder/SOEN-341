eaa4009edc108b848d3619e5a53fb0a84f76e869
Quang Jimmy <quang-jimmy@hotmail.com>
2019-02-10 22:37:00 -0500

    Finalized select semester page

--- client/src/flow-chart-pages/SelectSemester.js
+++ client/src/flow-chart-pages/SelectSemester.js
@@ -1,9 +1,17 @@
 import React from "react"
 import Navbar from "../components/Navbar"
-import {Dropdown} from "react-bootstrap"
+import { Dropdown, Button } from "react-bootstrap"
+import { Link } from "react-router-dom"
 
 class SelectSemester extends React.Component{
   render(){
+    const currentYear = (new Date()).getFullYear();
+    var yeetus = [];
+    for(let i=0;i<8;i++){ /*Basically choose a year from current year up to 8 years later. Don't touch this*/
+      yeetus[i] = currentYear + i;
+    }
+    const years = yeetus.map(jimmy => <Dropdown.Item>{jimmy}</Dropdown.Item>)
+
     return(
       <div className="container">
         <Navbar />
@@ -12,18 +20,35 @@ class SelectSemester extends React.Component{
           <div className="jumbotron j-greetings">
             <h2 className="display-4">Select Your Semester</h2>
             <hr color="#7e1530"/>
-
             <Dropdown>
               <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Dropdown Button
+                Select A Semester
               </Dropdown.Toggle>
 
               <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Fall</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Winter</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Summer</Dropdown.Item>
+                <Dropdown.Item>Fall</Dropdown.Item>
+                <Dropdown.Item>Winter</Dropdown.Item>
+                <Dropdown.Item>Summer</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
+
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Select A Year
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {years} {/*Array of year-component mapped*/}
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <Link to="/">
+              <Button variant="info">Continue</Button>
+            </Link>
+            <br />
+            <Link to="/">
+              <Button variant="danger">Back</Button>
+            </Link>
           </div>
         </div>
       </div>
