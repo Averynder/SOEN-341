bb4cc18ec22a870437681f3e978ccc8f53028c37
Niels Louis <46662488+NJLprograms@users.noreply.github.com>
2019-02-13 20:02:03 -0500

    Add files via upload
    NOT READY/FINAL
    Added changes to attempt to retrieve post method data from web page and add into database to test connection

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,6 +1,32 @@
 import React from 'react'
 
 class Modal extends React.Component{
+	  constructor() {
+      super()
+
+      this.state = { user: {} };
+      this.onSubmit = this.handleSubmit.bind(this);
+
+    }
+	 handleSubmit(e) {
+      e.preventDefault();
+      var self = this;
+      // On submit of the form, send a POST request with the data to the server.
+      fetch('/users', {
+          method: 'POST',
+          data: {
+            Netname: self.refs.netname,
+            Password: self.refs.password
+          }
+        })
+		
+        .then(function(response) {
+          return response.json()
+        }).then(function(body) {
+          console.log(body);
+        });
+    }
+	
     render(){
       if(!this.props.show){
         return null;
@@ -9,23 +35,22 @@ class Modal extends React.Component{
     return (
         <div className="container" id="modal-container">
           <div className="modal-content">
-
             <div className="modal-header">
               <h3>{this.props.userType}</h3>
               <button type="button" style={{margin: 0}} className="btn btn-dark" data-dismiss="modal" onClick={this.props.onClose}>&times;</button>
             </div>
-            <form>
+            <form onSubmit={this.onSubmit}>
               <div className="modal-body" style={{textAlign: "center"}}>
                 <h4>Sign In</h4>
                 <br />
                 <div class="md-form mb-5">
                   <h6 class="fas fa-envelope prefix grey-text">Netname:  </h6>
-                  <input className="input validate" type="text" id="defaultForm-email" placeholder="n_louis" required />
+                  <input className="input validate" type="text" id="defaultForm-email" ref = "netname" placeholder="n_louis" required />
                 </div>
 
                 <div class="md-form mb-5">
                   <h6 class="fas fa-envelope prefix grey-text">Password:  </h6>
-                  <input className="input validate" type="password" id="defaultForm-email"  placeholder="********" required />
+                  <input className="input validate" type="password" id="defaultForm-email" ref = "password" placeholder="********" required />
                 </div>
 
               </div>
