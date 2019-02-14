7d5599524435b69013f163b24bcc565419a0d021
Niels Louis <46662488+NJLprograms@users.noreply.github.com>
2019-02-14 03:42:16 -0500

    Update users.js
    Uncommented

--- routes/users.js
+++ routes/users.js
@@ -22,7 +22,7 @@ app.get('/', function(req, res, next) {
 	// Modify this query for the desired action on the database
 	connection.query('INSERT INTO `account user`(`Netname`, `Password`) values ("D0loresH4ze","samsepi0l")', function (error, results, fields) {
 	if (error) throw error;
-	res.send/*(JSON.stringify(results))-This will show the results in the web page itself, uncomment to try*/;
+	res.send(JSON.stringify(results));
 	});
 })
 
