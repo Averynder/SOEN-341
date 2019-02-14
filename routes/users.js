98d07a38fd27be867304c2d8cd6562fc78bc8569
Niels Louis <46662488+NJLprograms@users.noreply.github.com>
2019-02-13 19:59:28 -0500

    Add files via upload
    NOT READY
    Added changes to connection data and query to reflect and test new database
    Added function to listen to port 3000

--- routes/users.js
+++ routes/users.js
@@ -1,36 +1,51 @@
 var express = require('express');
 var router = express.Router();
-
+var app = express();
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
 	connectionLimit: 20,
     host: '127.0.0.1',
     user: 'root',
     password: 'password',
-    database: 'test'
+    database: 'soen341'
 });
-
 connection.connect();
-connection.query('select 1+1 as solution', function(error, results, fields) {
+
+
+connection.query('select* from `account user`', function(error, results, fields) {
     if(error) throw error;
-    console.log('The solution is: ', results[0].solution);
+    console.log('The solution is: ', results);
 });
 
 connection.end();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+// GET users listing. 
+router.get('/users', function(req, res, next) {
 	// Comment out this line:
-  //res.send('respond with a resource');
-
+  res.send('respond with a resource');
   // And insert something like this instead:
   res.json([{
-  	id: 1,
-  	username: "samsepi0l"
-  }, {
-  	id: 2,
-  	username: "D0loresH4ze"
+  	Netname: "D0loresH4ze",
+  	Password: "samsepi0l"
   }]);
+});
+
+
+
+// Listen to POST requests to /users.
+app.post('/users', function(req, res) {
+  // Get sent data.
+  var user = req.body;
+  // Do a MySQL query.
+  var query = connection.query('INSERT INTO `account user`(`Netname, `Password`)values("D0loresH4ze","samsepi0l")', user, function(err, result) {
+    // Neat!
+  });
+  res.end('Success');
+});
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000!');
 });
 
 module.exports = router;
