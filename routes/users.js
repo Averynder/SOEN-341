4de3c414c339ba6502c5fc80d0ad6b1de69cfaa0
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-02-17 13:54:20 -0500

    Use mysql2 library

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var mysql = require('mysql');
+var mysql = require('mysql2');
 var app = express();
 var connection = mysql.createConnection({
 	connectionLimit: 20,
