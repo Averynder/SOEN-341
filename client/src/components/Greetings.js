9b4067ccefe3a47e42911096407eee761288612f
Niels Louis <46662488+NJLprograms@users.noreply.github.com>
2019-02-25 18:07:39 -0500

    Update Greetings.js
    small edit

--- client/src/components/Greetings.js
+++ client/src/components/Greetings.js
@@ -7,12 +7,7 @@ function Greetings() {
         <h2 className="display-4">Hey Concordians!</h2>
         <hr color="#7e1530"/>
         <p className="lead">Welcome to Concordia Software Engineering Schedule Builder</p>
-    	  <p id="currentTime">
-           /* {setInterval(() => {
-            document.getElementById('currentTime').innerHTML = 
-            (new Date()).toLocaleString("en",{weekday:"long", month:"long", day:"numeric", year:"numeric", hour:"numeric", minute:"numeric", second:"numeric", hour12:false})},1000)
-          }*/
-        </p>
+    	  <p id="currentTime"></p>
       </div>
     </div>
   )
