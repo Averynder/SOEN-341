3973e915b59eada09cab0b6bd2c92783574416a4
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-02-28 14:02:52 -0500

    Add netname attribute to dummy user

--- seeders/20190214212037-demo-user.js
+++ seeders/20190214212037-demo-user.js
@@ -2,9 +2,11 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-      return queryInterface.bulkInsert('Users', [{
-          username: 'john'
-      }], {});
+		return queryInterface.bulkInsert('Users', [{
+			id: 100,
+			netname: 'user',
+			password: 'pass'
+		}], {});
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
@@ -18,6 +20,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
+		return queryInterface.bulkDelete('Users', null, {});
     /*
       Add reverting commands here.
       Return a promise to correctly handle asynchronicity.
