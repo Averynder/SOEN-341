7ff8aa60c18fb2edec2599e0674497af06c44971
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-02-28 14:29:28 -0500

    Change table name to Users from  and add logging to User.findOne()

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var mysql = require('mysql2');
 var app = express();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('soen341', 'root', 'password', {
+	host: 'localhost',
+	dialect: 'mysql'
+});
+const User = sequelize.import(__dirname + '/../models/user');
+
 var connection = mysql.createConnection({
 	connectionLimit: 20,
     host: '127.0.0.1',
@@ -10,19 +17,22 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
-connection.query('select * from `account user`', function(error, results, fields) {
+connection.query('select * from `Users`', function(error, results, fields) {
     if(error) throw error;
     console.log('The solution is: ', results);
 });
 
 
 
+
 /* GET users listing. */
 app.get('/', function(req, res, next) {
-	// Modify this query for the desired action on the database
-	connection.query('select `Password` from `account user`', function (error, results, fields) {
-	if (error) throw error;
-	console.log('Operation is: ', results);
+	User
+		.findOne()
+		.then(function(user) {
+			console.log(user.id);
+			console.log(user.netname);
+			console.log(user.password);
 	});
 	res.json([{
   	id: 1,
