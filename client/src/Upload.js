e0d93922c3976924b38161eb199d49798822986f
Ali Mahram <alimahram@hotmail.com>
2019-03-01 16:27:15 -0500

    for upload

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -20,7 +20,7 @@ class Upload extends React.Component {
   fileUploadHandler = () => {
     const fd = new FormData();
     fd.append('upload', this.state.selectedFile, this.state.selectedFile.name);
-    axios.post('http://localhost:3001', fd)
+    axios.post('/', fd)
       .then(res => {
         console.log(res);
       })
@@ -29,8 +29,8 @@ class Upload extends React.Component {
   render(){
     return(
         <div className="form-group">
-          <input type="file" className="btn btn-dark" onChange={this.fileSelectedHandler}/>
-          <button className="btn btn-dark" onClick={this.fileUploadHandler}>Upload</button>
+          <input type="file" name="filename" className="btn btn-dark" onChange={this.fileSelectedHandler}/>
+          <button className="btn btn-dark" value="upload" onClick={this.fileUploadHandler}>Upload</button>
         </div>
     )
   }
