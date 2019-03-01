374f638f86d48c333b12b26464b2b13d99def971
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-03-01 14:01:06 -0500

    Rename 'Users' table to 'account user'

--- models/user.js
+++ models/user.js
@@ -2,21 +2,20 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     netname: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
   }, {
+		tableName: "account user",
 		timestamps: false
 	});
   User.associate = function(models) {
     // associations can be defined here
   };
-	/*
 	User.sync({force: false}).then(() => {
 			return User.create({
 				netname: 'user',
 				password: 'pass'
 			});
 	});
-	*/
   return User;
 };
 
