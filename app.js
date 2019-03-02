691175fb30ab7a8cd1a2616912d50257686a46f8
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-03-02 17:07:02 -0500

    1-hour

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(session({
 	saveUninitialized: false,
 	resave: false,
 	cookie: {
-		maxAge: 1000 * 60 * 60 * 24 // 1 day
+		maxAge: 1000 * 60 * 60 // 1 hour
 	}
 }));
 app.use(logger('dev'));
