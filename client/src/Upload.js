f4d3ec743899dfa95315a575f7193829684e7be9
Ali Mahram <alimahram@hotmail.com>
2019-03-01 21:25:23 -0500

    issues 68 and 85

--- client/src/Upload.js
+++ client/src/Upload.js
@@ -2,35 +2,56 @@ import React from "react"
 import axios from "axios"
 
 
+const endpoint = 'http://localhost:3001/upload'
 class Upload extends React.Component {
 
   constructor() {
-    super();
+    super()
     this.state = {
       selectedFile: null,
+      loaded: 0,
     }
   }
 
-  fileSelectedHandler = event => {
+  handleSelectedFile = event => {
     this.setState({
-      selectedFile: event.target.files[0]
+      selectedFile: event.target.files[0],
+      loaded: 0,
     })
   }
 
-  fileUploadHandler = () => {
-    const fd = new FormData();
-    fd.append('upload', this.state.selectedFile, this.state.selectedFile.name);
-    axios.post('/', fd)
-      .then(res => {
-        console.log(res);
-      })
+  handleUpload = () => {
+    const data = new FormData();
+    data.append('file', this.state.selectedFile, this.state.selectedFile.name);
+    console.log(this.state.selectedFile);
+    var file = this.state.selectedFile,read = new FileReader();
+
+    read.readAsBinaryString(file);
+
+    read.onloadend = () => {
+      var rt = {"contentFile": read.result, "filename": this.state.selectedFile.name};
+      axios
+        .post(endpoint, rt, {
+          onUploadProgress: ProgressEvent => {
+            this.setState({
+              loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100,
+            })
+          },
+        })
+        .then(res => {
+          console.log(res.statusText);
+        })
+  }
+
+
   }
 
   render(){
     return(
         <div className="form-group">
-          <input type="file" name="filename" className="btn btn-dark" onChange={this.fileSelectedHandler}/>
-          <button className="btn btn-dark" value="upload" onClick={this.fileUploadHandler}>Upload</button>
+          <input type="file" name="filename" className="btn btn-dark" onChange={this.handleSelectedFile}/>
+          <button className="btn btn-dark" value="upload" onClick={this.handleUpload}>Upload</button>
+          <div> {Math.round(this.state.loaded, 2)} %</div>
         </div>
     )
   }
