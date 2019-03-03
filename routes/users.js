00740c53f8694395701d73c50bbd4c1f8799a94b
Averynder <averyndersingh@gmail.com>
2019-03-03 03:22:30 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,13 @@ var express = require('express');
 var mysql = require('mysql2');
 var app = express();
 var fs = require('fs');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('soen341', 'root', 'password', {
+	host: 'localhost',
+	dialect: 'mysql'
+});
+const User = sequelize.import(__dirname + '/../models/user');
+
 var connection = mysql.createConnection({
 	connectionLimit: 20,
     host: '127.0.0.1',
@@ -11,6 +18,10 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+connection.query('select * from `Users`', function(error, results, fields) {
+    if(error) throw error;
+    console.log('The solution is: ', results);
+});
 connection.query('SELECT*FROM `teacher`;', function(error, results, fields) {
     if(error) throw error;
     console.log('The solution is: ', results);
@@ -90,6 +101,26 @@ if (line.search(/(subject":".....,"catalog":.{6},)/) >= 0) {
 app.get('/niels', (req, res, next)=>{
 	res.json(time);
 });
+
+/* GET users listing. */
+app.get('/', function(req, res, next) {
+	User
+		.findOne()
+		.then(function(user) {
+			console.log(user.id);
+			console.log(user.netname);
+			console.log(user.password);
+	});
+	res.json([{
+  	id: 1,
+  	username: "samsepi0l"
+  }, {
+  	id: 2,
+  	username: "D0loresH4ze"
+  }]);
+})
+
+
 
 module.exports = app;
 connection.end;
