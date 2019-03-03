b9c9f5a53734ba73aca298642a65e1b44712a7ab
Averynder <averyndersingh@gmail.com>
2019-03-03 03:08:14 -0500

    Matching App for update

--- app.js
+++ app.js
@@ -13,17 +13,60 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var cookiesRouter = require('./routes/cookiesV');
-// var bodyParser = require('body-parser');
 var https = require('https');
 var readlineSync = require('readline-sync');
 var fs = require('fs');
-
+var bodyParser = require('body-parser');
 
 var app = express();
 
-app.use(cookieParser());
-app.use(session({secret: "Testing secret session."}));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(session({
+	secret: 'keyboard cat',
+	saveUninitialized: false,
+	resave: false,
+	cookie: {
+		maxAge: 1000 * 60 * 60 // 1 hour
+	}
+}));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(fileUpload());
+app.use(cors());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+var sequelize = require('./sequelize'); // get running instance of Sequelize
+require('./passport')(passport, sequelize); // importing passport.js with as a parameter the imported passport library from above
+
+app.post('/login', function(req, res, next) {
+	passport.authenticate('local', function(err, user, info) {
+		if (err) { return next(err); }
+		if (!user) {
+			res.end('/home');
+		} else {
+			req.login(user, function(error) {
+				res.end('/');
+			});
+		}
+	})(req, res, next);
+});
+
+app.get('/logout', (req, res, next) => {
+	req.logout();
+	res.end();
+});
+
+app.get('/check', function(req, res, next) {
+	if (req.user) { console.log('logged in'); }
+	if (!req.user) { console.log('logged out'); }
+	res.end();
+});
 
 // set a cookie with random number as ID
 // we should link this id to the given username/password input
@@ -53,10 +96,24 @@ app.get('/api', function(req, res){
 	res.json({
 		"visits": req.session.userVisits,
 		"cookie": req.cookies.cookieID,
-		"username": req.cookies.username,
+		"netname": req.cookies.netname,
 		"password": req.cookies.password
 	});
 });
+
+app.get('/concordia', function(req, res) {
+	https.get('https://172:0c35de81ea4c5cef9ee6073c3a6752eb@opendata.concordia.ca/API/v1/course/description/filter/000106', (response) => {
+		response.on('data', (d) => {
+			process.stdout.write(d);
+		});
+	}).on('error', (e) => {
+		console.log(e);
+	});
+	res.end();
+});
+
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 // Get Course Descriptions, to be reimplemented in regards to database refresh
 app.get('/concordia1', function(req, res) {
@@ -151,6 +208,7 @@ app.get('/concordia1', function(req, res) {
 	}						
 });
 
+/*
 passport.use(new LocalStrategy(
 	function(username, password, done) {
 		User.findOne({ username: username }, function(err, user) {
@@ -165,32 +223,15 @@ passport.use(new LocalStrategy(
 		});
 	}
 ));
-
-app.use('/public', express.static(__dirname + '/public'));
-
-
-//app.post('/login', passport.authenticate('local', { successRedirect: '/',
-                                                    //failureRedirect: '/login' }));
-
+*/
 
 // view engine setup (keep this)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(cors())
-app.use(bodyParser.json())
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(fileUpload())
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(passport.initialize());
-app.use(passport.session());
-
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/login', loginRouter);
+//app.use('/users', usersRouter);
+//app.use('/login', loginRouter);
 
 var fileUploaded = {};
 
@@ -219,9 +260,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
-
-
 
 module.exports = app;
