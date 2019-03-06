b452a9fe1caa0ed54de20e39db12957c6c773b1f
Ali Mahram <alimahram@hotmail.com>
2019-03-06 13:25:10 -0500

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,52 @@
-import React from "react"
-import Navbar from "./components/Navbar"
-import Greetings from "./components/Greetings"
-import ButtonContainer from "./components/ButtonContainer"
-import Modal from "./components/Modal"
-import Backdrop from "./components/Backdrop"
-import Button from "./components/Button"
-import {Link} from "react-router-dom"
-import './App.css';
-import LinkBox from "./LinkBox"
+import React from "react";
+import Navbar from "./components/Navbar";
+import Greetings from "./components/Greetings";
+import ButtonContainer from "./components/ButtonContainer";
+import Modal from "./components/Modal";
+import Backdrop from "./components/Backdrop";
+import Button from "./components/Button";
+import { Link } from "react-router-dom";
+import "./App.css";
+import LinkBox from "./LinkBox";
 /*import axios from 'axios';*/
 
-class App extends React.Component{
-	constructor() {
-		super();
-		
-		this.state ={
-			clock:[]
-		}
-		
-		this.state = {
-			isOpenStudent: false,
-			isOpenTeacher: false
-		}
+class App extends React.Component {
+  constructor() {
+    super();
 
-		this.toggleStudent = this.toggleStudent.bind(this);
-		this.toggleTeacher = this.toggleTeacher.bind(this);
-	}
+    this.state = {
+      clock: []
+    };
 
-	componentDidMount() {
-		fetch('/users')
-		.then(res => res.json())
-       	 	.then(users => this.setState({ users }));
-    	}
-	
-	
-	
-	
-	toggleStudent(){
-		this.setState({
-			isOpenStudent: !this.state.isOpenStudent
-		})
-	}
+    this.state = {
+      isOpenStudent: false,
+      isOpenTeacher: false
+    };
 
-	toggleTeacher(){
-		this.setState({
-			isOpenTeacher: !this.state.isOpenTeacher
-		})
-	}
+    this.toggleStudent = this.toggleStudent.bind(this);
+    this.toggleTeacher = this.toggleTeacher.bind(this);
+  }
 
-	render(){
-		/*
+  // componentDidMount() {
+  // 	fetch('/users')
+  // 	.then(res => res.json())
+  //    	 	.then(users => this.setState({ users }));
+  // 	}
+
+  toggleStudent() {
+    this.setState({
+      isOpenStudent: !this.state.isOpenStudent
+    });
+  }
+
+  toggleTeacher() {
+    this.setState({
+      isOpenTeacher: !this.state.isOpenTeacher
+    });
+  }
+
+  render() {
+    /*
 		axios.get('/users')
 		.then(res => {
 			var clock =res.data;
@@ -57,30 +54,39 @@ class App extends React.Component{
 			})
 		.then(setInterval(() => {document.getElementById('currentTime').innerHTML = this.state.clock},1000))
 		*/
-		return (
-			<div className="bckgrnd container">
-				<Navbar />
-				<Greetings/>
-				<ButtonContainer>
-					<Button text="I Am A Student" onClick={this.toggleStudent}/>
-					<Button text="I Am A Professor" onClick={this.toggleTeacher}/>
-					<Link to="/build-seq-or-sem">
-						<Button text="I Am New To This Website" />
-					</Link>
-				</ButtonContainer>
+    return (
+      <div className="bckgrnd container">
+        <Navbar />
+        <Greetings />
+        <ButtonContainer>
+          <Button text="I Am A Student" onClick={this.toggleStudent} />
+          <Button text="I Am A Professor" onClick={this.toggleTeacher} />
+          <Link to="/build-seq-or-sem">
+            <Button text="I Am New To This Website" />
+          </Link>
+        </ButtonContainer>
 
-				<Backdrop show={this.state.isOpenStudent} onClose={this.toggleStudent}>
-					<Modal show={this.state.isOpenStudent} onClose={this.toggleStudent} userType="Student" link="/previous-courses-taken"/>
-				</Backdrop>
+        <Backdrop show={this.state.isOpenStudent} onClose={this.toggleStudent}>
+          <Modal
+            show={this.state.isOpenStudent}
+            onClose={this.toggleStudent}
+            userType="Student"
+            link="/previous-courses-taken"
+          />
+        </Backdrop>
 
-				<Backdrop show={this.state.isOpenTeacher} onClose={this.toggleTeacher}>
-					<Modal show={this.state.isOpenTeacher} onClose={this.toggleTeacher} userType="Teacher" link="/pull-previous-courses"/>
-				</Backdrop>
-				<LinkBox />
-			</div>
-		)
-	}
+        <Backdrop show={this.state.isOpenTeacher} onClose={this.toggleTeacher}>
+          <Modal
+            show={this.state.isOpenTeacher}
+            onClose={this.toggleTeacher}
+            userType="Teacher"
+            link="/pull-previous-courses"
+          />
+        </Backdrop>
+        <LinkBox />
+      </div>
+    );
+  }
 }
 
-export default App
-
+export default App;
