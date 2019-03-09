1278eae9a6db01694e0d15c4a44cd3d2f8460081
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-03-09 16:04:21 -0500

    Comment out code unrelated to login #86

--- selenium.js
+++ selenium.js
@@ -13,12 +13,10 @@ module.exports = (app) => {
         driver.get('https://my.concordia.ca/')
           .then(_ => driver.findElement(By.name('userid')).sendKeys(req.params.netname))
           .then(_ => driver.findElement(By.name('pwd')).sendKeys(req.params.password, Key.RETURN))
-          .then(_ => driver.wait(until.elementLocated(By.xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/ul/li[1]/table/tbody/tr[2]/td/div/nav/div[1]/ul/li[2]/div')), 4000), (e) => {
-            console.log('failed login');
-          })
-          .then(_ => driver.findElement(By.xpath('//*[@id="CU_MY_STUD_CENTRE"]/div')).click())
-          .then(_ => driver.wait(until.elementLocated(By.xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/ul/li[1]/table/tbody/tr[2]/td/div/nav/div[1]/ul/li[2]/ul/li[1]/a')), 4000))
-          .then(_ => driver.findElement(By.xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/ul/li[1]/table/tbody/tr[2]/td/div/nav/div[1]/ul/li[2]/ul/li[1]/a')).click())
+          .then(_ => driver.wait(until.elementLocated(By.xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/ul/li[1]/table/tbody/tr[2]/td/div/nav/div[1]/ul/li[2]/div')), 4000))
+          //.then(_ => driver.findElement(By.xpath('//*[@id="CU_MY_STUD_CENTRE"]/div')).click()) // click on the arrow to show MyStudentCentre
+          //.then(_ => driver.wait(until.elementLocated(By.xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/ul/li[1]/table/tbody/tr[2]/td/div/nav/div[1]/ul/li[2]/ul/li[1]/a')), 4000)) // wait until content shows up
+          //.then(_ => driver.findElement(By.xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[1]/ul/li[1]/table/tbody/tr[2]/td/div/nav/div[1]/ul/li[2]/ul/li[1]/a')).click()) // click on MyStudent Centre
           //.then(_ => driver.wait(until.elementLocated(By.xpath('//*[@id="DERIVED_SSS_SCL_DESCR254A"]')), 8000)) // body of MyStudent Centre
           //.then(_ => driver.findElement(By.xpath('//*[@id="DERIVED_SSS_SCL_TITLE1$78$"]')).getText()) // Netname
           .then(_ => driver.quit(), e => driver.quit().then(() => { throw e; }));
