9e013b0eb7620bfcedd0dc7e122f522df0754dc9
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-03-24 19:11:42 -0400

    Add first name

--- client/src/BuildSeqOrSem.js
+++ client/src/BuildSeqOrSem.js
@@ -5,27 +5,41 @@ import Navbar from "./components/Navbar"
 
 class BuildSeqOrSem extends Component {
 
-    render() {
-      return (
+  componentDidMount() {
+    fetch("/grades")
+      .then(body => {
+        return body.json();
+      })
+      .then(info => {
+        console.log(info);
+        let elemFirstName = document.getElementById('firstName');
+        let valFirstName = info.result.studentInfo.givenName;
+        console.log(valFirstName);
+        elemFirstName.innerHTML = valFirstName;
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+          <Navbar />
         <div className="container">
-            <Navbar />
-          <div className="container">
-            <div className="jumbotron j-greetings">
-              <h2 className="display-4">Which build option would you like?</h2>
-              <hr color="#7e1530"/>
-              <div class="btn-group" role="group" aria-label="Basic example">
-                <Link to="/select-semester">
-                  <Button text="Semester"/>
-                </Link>
-                <Link to="/obtain-uploaded-sequence">
-                  <Button text="Sequence"/>
-                </Link>
-              </div>
+          <div className="jumbotron j-greetings">
+            <h2 className="display-4">Hi <span id="firstName"></span>, which build option would you like?</h2>
+            <hr color="#7e1530"/>
+            <div class="btn-group" role="group" aria-label="Basic example">
+              <Link to="/select-semester">
+                <Button text="Semester"/>
+              </Link>
+              <Link to="/obtain-uploaded-sequence">
+                <Button text="Sequence"/>
+              </Link>
             </div>
           </div>
         </div>
-      )
-    }
+      </div>
+    )
+  }
 }
 
 export default BuildSeqOrSem
