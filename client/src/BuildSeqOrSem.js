10d16fe0cc1fcf8c44a119a97ba512b5f6db2cd9
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-03-25 01:28:02 -0400

    Try to fix bug where changing routes doesn't show button

--- client/src/BuildSeqOrSem.js
+++ client/src/BuildSeqOrSem.js
@@ -7,35 +7,20 @@ class BuildSeqOrSem extends Component {
   constructor(props, context) {
     super(props, context);
 
-    this.state = {
-      hasLoggedIn: false,
-    };
+    this.state = { firstName: '' };
   }
 
-  componentDidMount() {
-    fetch("/grades")
-      .then(body => {
-        if (body.status === 200) {
-          this.setState({ hasLoggedIn: true });
-          return body.json();
-        } else {
-          return 'visitor';
-        }
-      })
-      .then(info => {
-        let elemFirstName = document.getElementById('firstName');
-        let valFirstName = info === 'visitor'? info: info.result.studentInfo.givenName;
-        elemFirstName.innerHTML = valFirstName;
-      });
+  onUpdate = val => {
+    this.setState({ firstName: val });
   }
 
   render() {
     return (
       <div className="container">
-          <Navbar hasLoggedIn={this.state.hasLoggedIn} />
+          <Navbar onUpdate={this.onUpdate} />
         <div className="container">
           <div className="jumbotron j-greetings">
-            <h2 className="display-4">Hi <span id="firstName"></span>, which build option would you like?</h2>
+            <h2 className="display-4">Hi {this.state.firstName}, which build option would you like?</h2>
             <hr color="#7e1530"/>
             <div class="btn-group" role="group" aria-label="Basic example">
               <Link to="/select-semester">
