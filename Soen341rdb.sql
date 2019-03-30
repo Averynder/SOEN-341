0616f07f19525f5f858e15b240c32f13e63fd3ee
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-03-30 12:41:32 -0400

    Remove unused table

--- Soen341rdb.sql
+++ Soen341rdb.sql
@@ -6,14 +6,6 @@
 CREATE SCHEMA IF NOT EXISTS `soen341`;
 USE `soen341` ;
 
-
- -- ---------------------------------------------------
- -- Table `soen341`.`account user`
- -- ---------------------------------------------------
- CREATE TABLE  `soen341`.`account user` (
-   `netname` varchar(50) PRIMARY KEY,
-   `password` varchar(50) DEFAULT NULL);
-
 -- -----------------------------------------------------
 -- Table `soen341`.`teacher`
 -- -----------------------------------------------------
--- routes/users.js
+++ routes/users.js
@@ -42,14 +42,6 @@ var connection = mysql.createConnection({
 //connection.connect();
 // Issue is not the fact of doing this retrieval, it's the actual timing of the thread
 
-connection.connect(function(err) {
-  if (err) throw err;
-  connection.query("SELECT * FROM `account user`", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-});
-
 // connection.query("SELECT*FROM `teacher`;", function(error, results, fields) {
 //   if (error) throw error;
 //   console.log("The solution is: ", results);
