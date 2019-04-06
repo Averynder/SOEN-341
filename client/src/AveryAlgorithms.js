d265469b5b3c06470569df56a4a603db67621e71
Rubiat <rubiat.zaman@gmail.com>
2019-04-06 15:21:33 -0400

    Time conflict works for single course

--- client/src/AveryAlgorithms.js
+++ client/src/AveryAlgorithms.js
@@ -21,6 +21,7 @@ class AveryAlgorithms extends Component {
 		this.stacks2arrays = this.stacks2arrays.bind(this);
 		this.timeConflict1 = this.timeConflict1.bind(this);
 		this.timeConflict = this.timeConflict.bind(this);
+		this.timeConflict2 = this.timeConflict2.bind(this);
 	}
 
 	duplicateArray(oldArray)
@@ -437,18 +438,99 @@ class AveryAlgorithms extends Component {
 		return false;
 	}
 
+	timeConflict2(course1)
+	{
+		let lectureIndex1 = course1[1];
+		let tutorialIndex1 = course1[2];
+		let labIndex1 = course1[3];
+
+		let lecture1, tutorial1, lab1;
+
+		if (lectureIndex1 != undefined) {
+			lecture1 = course1[0].lecture[lectureIndex1];
+		}
+		else {
+			lecture1 = course1.lecture[0];
+		}
+
+		if (tutorialIndex1 != null) {
+			tutorial1 = course1[0].lecture[lectureIndex1].tutorial[tutorialIndex1];
+		}
+		else {
+			tutorial1 = course1.lecture[0].tutorial[0];
+		}
+
+		if (labIndex1 != null) {
+			lab1 = course1[0].lab[labIndex1];
+		}
+		else {
+			lab1 = course1.lab[0];
+		}
+
+		let start1, start2, end1, end2;
+
+		for (let i = 0; i < lecture1.days.length; i++) { // compare lec1 to lab1
+			start1 = this.timeToNum(lecture1.startTime);
+			end1 = this.timeToNum(lecture1.endTime);
+
+			if (lab1 != undefined) {
+				for (let m = 0; m < lab1.days.length; m++) {
+					if (lecture1.days[i] == lab1.days[m]) {
+						start2 = this.timeToNum(lab1.startTime);
+						end2 = this.timeToNum(lab1.endTime);
+
+						if (((start2 > start1) && (!(start2 >= end1))) || ((!(start2 > start1)) && (!(start1 >= end2)))) {
+							//console.log("conflict between lecture1 and lab1");
+							return true;
+						}
+					}
+				}
+			}
+		}
+
+		if (tutorial1 != undefined) { // compare tut1 to lab1
+			for (let j = 0; j < tutorial1.days.length; j++) {
+				start1 = this.timeToNum(tutorial1.startTime);
+				end1 = this.timeToNum(tutorial1.endTime);
+
+				if (lab1 != undefined) {
+					for (let m = 0; m < lab1.days.length; m++) {
+						if (tutorial1.days[j] == lab1.days[m]) {
+							start2 = this.timeToNum(lab1.startTime);
+							end2 = this.timeToNum(lab1.endTime);
+
+							if (((start2 > start1) && (!(start2 >= end1))) || ((!(start2 > start1)) && (!(start1 >= end2)))) {
+								//console.log("conflict between tutorial1 and lab1");
+								return true;
+							}
+						}
+					}
+				}
+			}
+		}
+
+		return false;
+	}
+
 	timeConflict(courses) // takes array of courses and sees if there's conflicts between them
 	{
-		for (let i = 0; i < courses.length; i++) {
-			for (let j = i + 1; j < courses.length; j++) {
-				if (this.timeConflict1(courses[i], courses[j])) {
-					// try {
-					// 	console.log("conflict between " + courses[i][0].course + " and " + courses[j][0].course);
-					// }
-					// catch (err) {
-					// 	console.log("conflict between " + courses[i].course + " and " + courses[i].course); 
-					// }
-					return true;
+		if (courses.length == 1) {
+			if (this.timeConflict2(courses[0])) {
+				return true;
+			}
+		}
+		else {
+			for (let i = 0; i < courses.length; i++) {
+				for (let j = i + 1; j < courses.length; j++) {
+					if (this.timeConflict1(courses[i], courses[j])) {
+						// try {
+						// 	console.log("conflict between " + courses[i][0].course + " and " + courses[j][0].course);
+						// }
+						// catch (err) {
+						// 	console.log("conflict between " + courses[i].course + " and " + courses[i].course); 
+						// }
+						return true;
+					}
 				}
 			}
 		}
