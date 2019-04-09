ce617319549bbdf1c791d91932d67bcb0f058526
Andre Parsons-Legault <andre.legault1@gmail.com>
2019-04-08 22:17:16 -0400

    Remove "Test cases" directory

--- Test cases/src/test/java/UserCase29.java
+++
@@ -1,9 +0,0 @@
-public class UserCase29  extends  UserCase14{
-	public static boolean run (String user, String pass, boolean wantLogin){
-		boolean addedCourse =  UserCase22.run(user, pass, wantLogin);
-		if (! addedCourse)
-			return false;
-		else
-			return true; //incomplete, will need to remove courses
-	}
-}
